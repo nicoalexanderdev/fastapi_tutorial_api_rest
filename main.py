88331e52f13044bd0571fa33ee16aacabf7eabe5
Nicolás Oses <127158636+nicoalexanderdev@users.noreply.github.com>
2025-02-03 12:48:44 -0300

    Update main.py
    add new methods for blogs administration

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 
 app = FastAPI()
 
 app.title = "API Rest blog"
 app.version = "0.1.1"
 
-blog = [
+blog_list = [
     {
         "id": 1,
         "title": "Estadistica para Machine Learning",
         "subtitle": "Este es un blog para...",
         "author": "Nicolas Oses",
-        "fecha": "3/2/2025"
+        "date": "3/2/2025",
+        "category": "data science"
+    },
+    {
+        "id": 2,
+        "title": "Estadistica para Machine Learning",
+        "subtitle": "Este es un blog para...",
+        "author": "Nicolas Oses",
+        "date": "3/2/2025",
+        "category": "programacion"
     }
 ]
 
@@ -21,4 +30,32 @@ async def root():
 
 @app.get("/blogs", tags=['Blog'])
 async def all_blogs():
-    return blog
+    return blog_list
+
+@app.get("/blogs/{id}", tags=['Blog'])
+async def get_blog(id: int):
+    for blog in blog_list:
+        if blog['id'] == id:
+            return blog
+    return []
+
+@app.get("/blogs/", tags=['Blog'])
+async def get_blog_by_category(category: str):
+    for blog in blog_list:
+        if blog['category'] == category:
+            return blog
+    return []
+
+@app.post("/blogs", tags=['Blog'])
+async def create_blog(id: int = Body(), title: str = Body(), subtitle: str = Body(), author: str = Body(), date: str = Body(), category: str = Body()):
+    blog_list.append(
+        {
+            "id" : id,
+            "title" : title,
+            "subtitle" : subtitle,
+            "author" : author,
+            "date" : date,
+            "category" : category
+        }
+    )
+    return blog_list
